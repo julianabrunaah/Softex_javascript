f9522627107eed22fc8d0a5b9b61c3acdaf50a4b
Juliana S <julianabrunahh@gmail.com>
2022-08-01 15:58:03 -0300

    Update and rename terceiraNota.js to media.js
    Instruções do projeto
    Crie dois códigos de sistema de notas para uma escola. O primeiro código deve ser um programa que informa se o aluno reprovou, ou não, com base nas três notas que ele adicionou ao programa. Utilize, no mínimo, um operador de atribuição e um operador ternário. 
    O segundo código é um programa que o aluno deve escrever duas notas e o retorno informa a nota mínima que ele deve tirar na próxima prova para poder passar com nota sete.

--- Modulo 2/terceiraNota.js
+++
@@ -1,8 +0,0 @@
-function media(n1, n2){
-    var n3 = 21 - (n1 += n2)
-    return n3
-}
-var n1 = 6;
-var n2 = 7;
-var result = media(n1, n2)
-console.log("Para passar com media 7 é preciso tirar " +result, " na sua proxíma prova")
