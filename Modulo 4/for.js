e835fcc78d5bf4417b75a90897cf0defe5e852f1
Juliana S <65259666+clebits@users.noreply.github.com>
2022-08-15 15:11:42 -0300

    Update for.js

--- Modulo 4/for.js
+++ Modulo 4/for.js
@@ -1,13 +1,21 @@
 const array1 = [25, 45, 30]
 let clientes = {
     nome: "maria",
-    rg: "6.090.000"
+    rg: "6.090.000",
+    dataNascimento: "08/07/1998"
 }
 //imprime os valores do array
-for (const e of array1) {
-    console.log(e);
+function forOf (){
+    for (const e of array1) {
+        console.log(e);
+    }
 }
 //imprime as propriedades do obj
-for (let dados in clientes){
-    console.log(dados + ":"+ clientes[dados]);
+function forIn(){
+    for (let dados in clientes){
+        console.log(dados + ":"+ clientes[dados]);
+    }
 }
+
+forIn()
+forOf()
